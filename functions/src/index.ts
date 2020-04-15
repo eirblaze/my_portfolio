5b395a6ffc9dd5bf2004621ac48ce7366452c8a4
eirblaze <eirblaze@live.jp>
2020-04-15 11:39:26 +0900

    expressを構成

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,20 @@
 import * as functions from 'firebase-functions'
-// import * as express from 'express'
+import * as express from 'express'
 // import * as basicAuth from 'express-basic-auth'
 
-// const app = express()
+const app = express()
 
 // app.use(basicAuth({
 //   users: { 'admin': 'supersecret' }
 // }))
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+app.use(express.static(__dirname + '/static/'))
+
+exports.app = functions.https.onRequest(app)
