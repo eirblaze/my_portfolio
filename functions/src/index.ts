acb9fc2eaaddf12a50b28090f5d268e1240a82c2
eirblaze <eirblaze@live.jp>
2020-04-15 17:59:21 +0900

    ベーシック認証設定成功

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,22 @@
 import * as functions from 'firebase-functions'
 import * as express from 'express'
 // import * as path from 'path'
-// import * as basicAuth from 'express-basic-auth'
+import * as basicAuth from 'express-basic-auth'
 
 const app = express()
 
-app.all('/*', function (req, res) {
-  res.send('Hello express from Firebase!')
-})
+// つかいかた : https://qiita.com/hiroyky/items/a16dc3315b06fd04c72a
+// 型 : https://github.com/LionC/express-basic-auth/blob/master/express-basic-auth.d.ts
+app.use(basicAuth({
+  challenge: true,
+  unauthorizedResponse: () => {
+      return "Unauthorized" // 認証失敗時に表示するメッセージ
+  },
+  users: { someuser: "passstring" }
+}))
+
+// app.all() : https://expressjs.com/ja/4x/api.html#app.all
+// app.use() : https://expressjs.com/ja/4x/api.html#app.use
 
 // app.use(basicAuth({
 //   users: { 'admin': 'supersecret' }
@@ -18,6 +27,10 @@ app.all('/*', function (req, res) {
 // }))
 
 // app.use(express.static( path.resolve(__dirname + '/static/') ))
+
+app.use('/', function (req, res) {
+  res.send('Hello express from Firebase!')
+})
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
