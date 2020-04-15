47006bdb3f985966909f9131b75159c12a7ad620
eirblaze <eirblaze@live.jp>
2020-04-15 11:28:22 +0900

    ts対応のベーシック認証の仕組みを追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,12 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import * as express from 'express'
+import * as basicAuth from 'express-basic-auth'
+
+const app = express()
+
+app.use(basicAuth({
+  users: { 'admin': 'supersecret' }
+}))
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
