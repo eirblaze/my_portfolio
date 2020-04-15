030b7ca613ab79674817119e2842ea0adecbe479
eirblaze <eirblaze@live.jp>
2020-04-15 12:35:24 +0900

    検証２

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,13 @@
 import * as functions from 'firebase-functions'
-import * as express from 'express'
-import * as path from 'path'
+// import * as express from 'express'
+// import * as path from 'path'
 // import * as basicAuth from 'express-basic-auth'
 
-const app = express()
+// const app = express()
 
-app.all('/*', function (req, res) {
-  res.send('Hello express from Firebase!')
-})
+// app.all('/*', function (req, res) {
+//   res.send('Hello express from Firebase!')
+// })
 
 // app.use(basicAuth({
 //   users: { 'admin': 'supersecret' }
@@ -17,7 +17,7 @@ app.all('/*', function (req, res) {
 //   users: { 'admin': 'supersecret' }
 // }))
 
-app.use(express.static( path.resolve(__dirname + '/static/') ))
+// app.use(express.static( path.resolve(__dirname + '/static/') ))
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
