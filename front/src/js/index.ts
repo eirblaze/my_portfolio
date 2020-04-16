2198e596ef194feabeae2876cee49108290f5dd1
eirblaze <eirblaze@live.jp>
2020-04-16 10:31:31 +0900

    関数設置

--- front/src/js/index.ts
+++ front/src/js/index.ts
@@ -1,4 +1,6 @@
 import Sectioninfo from "./sectioninfo"
+import scroll from "./scroll"
+import Fade from "./fade"
 
 import "../sass/index.sass"
 
@@ -8,3 +10,12 @@ const sectioninfo = new Sectioninfo([
     target: $("span"),
   },
 ])
+
+scroll()
+const _fade: Fade[] = new Array(0)
+_fade.push(new Fade({
+  css_panel: ".section1",
+  obj_name: "section1",
+  css_clone_hide__pc: {},
+  css_clone_hide__sp: {}
+}))
--- front/src/js/scroll.ts
+++ front/src/js/scroll.ts
@@ -1,11 +1,5 @@
 import on_click_anchor from "./scroll/on_click_anchor"
 
 export default () => {
-  jQmain(jQuery)
-}
-
-function jQmain($:JQueryStatic) {
-  $(document).ready( () => {
-    on_click_anchor()
-  })
+  on_click_anchor()
 }
