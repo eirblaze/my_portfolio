5aef8b789d2b032533f5f8a92b45d5105f534de8
eirblaze <eirblaze@live.jp>
2020-04-17 18:42:43 +0900

    アンカーリンクのところまで作成

--- front/src/js/scroll/anchor_scroll.ts
+++ front/src/js/scroll/anchor_scroll.ts
@@ -1,9 +1,5 @@
 // 特定の位置までスクロールさせる
 export default ($target:JQuery,i_offset:number) => {
-  jQmain(jQuery,$target,i_offset)
-}
-
-function jQmain($:JQueryStatic,$target:JQuery,i_offset:number) {
 
   console.log(i_offset)
 
